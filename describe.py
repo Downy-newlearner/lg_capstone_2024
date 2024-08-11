ae2c393e3145e8134551191824039c35ad0c4a6b
Downy-newlearner <jdh251425142514@gmail.com>
2024-08-11 22:32:08 +0900

    모듈 적용 성공 및 본격적인 전처리 시작

--- describe.py
+++ describe.py
@@ -1,9 +1,16 @@
-#위 txt 파일로 저장하는 구문을 함수로 만들어서 사용
-def describe_df(df):
-    with open(f'./datas/df_summary.txt', 'w') as f:
-        for i in range(len(df.columns)):
+import os
 
-            f.write(f'{i+1}: {df.columns[i]}:\nnunique: ' + \
-                    str(df[df.columns[i]].nunique()) + "\n" \
-                        + str(df[df.columns[i]].unique()) + "\n\n")
-    print('저장완료')
+def describe_df(df, directory=None):
+    # 디렉토리가 존재하지 않으면 생성
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # 파일 경로 지정
+    file_path = os.path.join(directory, 'df_summary.txt')
+
+    # 파일 쓰기
+    with open(file_path, 'w') as f:
+        for i in range(len(df.columns)):
+            f.write(f'{i + 1}: {df.columns[i]}:\nnunique: ' +
+                    str(df[df.columns[i]].nunique()) + "\n" +
+                    str(df[df.columns[i]].unique()) + "\n\n")
